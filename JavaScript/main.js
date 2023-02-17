1804d0bd6a4a0b7ee0096f36317a20cbc02aa615
Mateusz Czarnecki <mateuszczarnecki360@gmail.com>
2023-02-17 16:07:30 +0100

    Update main.js

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,14 +1,21 @@
-require('dotenv').config({ path: '../.env' });
-const axios = require('axios');
+require("dotenv").config({ path: "../.env" });
+const axios = require("axios");
 
 const apiKey = process.env.API_KEY;
 const city = "Warsaw";
 
-axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`)
-    .then(response => {
+axios
+    .get(
+        `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+    )
+    .then((response) => {
         const tempCelsius = response.data.main.temp - 273.15;
-        console.log(`Current temperature in ${city}: ${tempCelsius.toFixed(2)} degrees Celsius`);
+        console.log(
+            `Current temperature in ${city}: ${tempCelsius.toFixed(
+                2
+            )} degrees Celsius`
+        );
     })
-    .catch(error => {
+    .catch((error) => {
         console.error(`Error getting weather data: ${error}`);
     });
